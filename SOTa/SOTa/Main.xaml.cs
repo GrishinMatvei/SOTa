de20a29666c56d55f660197ae443c99badfaf0f5
GrishinMatvei <113914375+GrishinMatvei@users.noreply.github.com>
2022-11-02 13:57:07 +0300

    Никита лох

--- SOTa/SOTa/Main.xaml.cs
+++ SOTa/SOTa/Main.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SOTa
 {
@@ -10,6 +11,11 @@ namespace SOTa
         TradeEntities db = new TradeEntities();
 
         User user;
+
+        private readonly Boolean IsInit = false;
+
+        Domain.Product[] Products = new Domain.Product[] { };
+
         public Main(User user1)
         {
             user = user1;
@@ -19,14 +25,17 @@ namespace SOTa
 
             userInfo.Text = $"{user1.UserSurname} {user1.UserName[0]}. {user1.UserPatronymic[0]}.";
 
-            products.ItemsSource = ProductConverter.ToProducts(TradeEntities.GetContext().Product.ToArray()); 
+            Products = ProductConverter.ToProducts(TradeEntities.GetContext().Product.ToArray());
+            productList.ItemsSource = Products;
+
+            IsInit = true;
         }
 
         private void products_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             try
             {
-                var f = products.SelectedItem;
+                var f = productList.SelectedItem;
 
                 if (f != null && user.Role.RoleID == Convert.ToInt32(Enums.Role.Administator))
                 {
@@ -43,7 +52,7 @@ namespace SOTa
 
         private void deleteItem_Click(object sender, RoutedEventArgs e)
         {
-            var f = products.SelectedValue.ToString();
+            var f = productList.SelectedValue.ToString();
 
             Product delProduct = db.Product.SingleOrDefault(n => n.ProductArticleNumber == f);
 
@@ -52,11 +61,11 @@ namespace SOTa
                 if (MessageBox.Show("Вы действительно хотите удалить запись?", "Удалить", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
                 {
                     db.Product.Remove(delProduct);
-                    products.SelectedItem = null;
+                    productList.SelectedItem = null;
                     db.SaveChanges();
                     deleteItem.Visibility = Visibility.Hidden;
                     editItem.Visibility = Visibility.Hidden;
-                    products.ItemsSource = ProductConverter.ToProducts(TradeEntities.GetContext().Product.ToArray());
+                    productList.ItemsSource = ProductConverter.ToProducts(TradeEntities.GetContext().Product.ToArray());
                 }
             }
         }
@@ -68,8 +77,8 @@ namespace SOTa
         }
 
         private void editItem_Click(object sender, RoutedEventArgs e)
-        {          
-            Product product = db.Product.SingleOrDefault(n => n.ProductArticleNumber == products.SelectedValue);
+        {
+            Product product = db.Product.SingleOrDefault(n => n.ProductArticleNumber == productList.SelectedValue);
 
             EditProduct editProduct = new EditProduct(product);
             editProduct.ShowDialog();
@@ -80,6 +89,43 @@ namespace SOTa
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
             Close();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (!IsInit) return;
+
+            ComboBox comboBox = (ComboBox)sender;
+
+            Domain.Product[] products = new Domain.Product[] { };
+
+            switch (comboBox.SelectedValue)
+            {
+                case "withoutSort":
+                    {
+                        products = Products;
+                        break;
+                    }
+                case "ascending":
+                    {
+                        products = Products.OrderBy(product => product.ProductCost).ToArray();
+                        break;
+                    }
+                case "descending":
+                    {
+                        products = Products.OrderByDescending(product => product.ProductCost).ToArray();
+                        break;
+                    }
+            }
+
+            if (products.Length == 0) MessageBox.Show("Не удалось загрузить данные");
+
+            productList.ItemsSource = products;
+        }
+
+        private void search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            productList.ItemsSource = Products.Where(w => w.ManufacturerName.ToLower().Contains(search.Text.ToLower()) || w.ProductName.ToLower().Contains(search.Text.ToLower()) || w.ProductDescription.ToLower().Contains(search.Text.ToLower()) || w.ProductName.ToLower().Contains(search.Text.ToLower()) ).ToArray();  
         }
     }
 }
